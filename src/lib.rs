d14729cc827208abe04841064a8f630a031b7c23
Spencer Nelson <s@spencerwnelson.com>
2023-12-14 16:59:38 -0800

    Version 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -12,23 +12,23 @@ mod tests {
     #[test]
     fn test_create_simulation() {
         unsafe {
-            let sim = reb_create_simulation();
+            let sim = reb_simulation_create();
             assert!(!sim.is_null());
-            reb_free_simulation(sim);
+            reb_simulation_free(sim);
         }
     }
 
     #[test]
     fn test_integrate_particle() {
         unsafe {
-            let sim = reb_create_simulation();
+            let sim = reb_simulation_create();
             let fmt_str = CString::new("M a").unwrap();
-            let particle = reb_particle_new(sim, fmt_str.as_ptr(), 1, 1);
-            reb_add(sim, particle);
+            let particle = reb_particle_from_fmt(sim, fmt_str.as_ptr(), 1, 1);
+            reb_simulation_add(sim, particle);
 
-            reb_integrate(sim, 1.0);
+            reb_simulation_integrate(sim, 1.0);
 
-            reb_free_simulation(sim);
+            reb_simulation_free(sim);
         }
     }
 }
